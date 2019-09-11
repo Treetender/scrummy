aa969645a21769edaf2bcc4b1c802b9f5c311707
Treetender <treetender@gmail.com>
2019-09-11 12:24:22 -0400

    on click added to selected card to go back to list

--- src/app/selected-card/selected-card.component.ts
+++ src/app/selected-card/selected-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-selected-card',
@@ -11,10 +12,14 @@ export class SelectedCardComponent implements OnInit {
   private textSubject$: BehaviorSubject<string> = new BehaviorSubject('');
   readonly text$ = this.textSubject$.asObservable();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
-    this.route.params.subscribe(param => this.textSubject$.next(param.text)); 
+    this.route.params.subscribe(param => this.textSubject$.next(param.text));
+  }
+  
+  onClick() {
+    this.location.back();
   }
 
 }
